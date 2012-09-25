ad5a166ea6e8eb0981189a651551063f6d9fc7dd
Pete Gadomski <pete.gadomski@gmail.com>
2012-09-25 13:59:23 -0700

    Add author_biography

--- app.rb
+++ app.rb
@@ -24,6 +24,21 @@ module Nesta
       end
     end
 
-    # Add new routes here.
+    def author_biography(name = nil)
+      name ||= @page.metadata('author')
+      if name
+        short_name = name.downcase.gsub(/\W+/, '_').to_sym
+        avatar_path = File.join(['images', 'authors', "#{short_name}.jpg"])
+        html = ""
+        locals = { :has_avatar => false }
+        if File.exist?(File.join(Nesta::App.root, 'public', avatar_path))
+          html += capture_haml do
+            haml_tag :img, :src => "/#{avatar_path}", :class => 'avatar'
+          end
+          locals[:has_avatar] = true
+        end
+        html << haml(short_name.to_sym, :layout => false, :locals => locals)
+      end
+    end
   end
 end
